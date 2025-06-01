91a00245af0d7ddeb7576cf29e95653a3c2e90d4
UsopKing7 <programadorusop@gmail.com>
2025-06-01 00:48:09 -0400

    refactor: reorganizar interfaces en config.ts para mayor claridad

--- Backend/src/config.ts
+++ Backend/src/config.ts
@@ -1,7 +1,19 @@
+import { QueryResultRow } from 'pg'
+
+// interface para Port del servidor
 interface Port {
   Port: number
 }
 
 export const PORT: Port = {
   Port: Number(process.env.PORT) || 3333
+}
+
+// interface para Usuarios en consulta
+export interface UsuarioConsulta extends QueryResultRow {
+  id: `${string}-${string}-${string}-${string}-${string}`
+  username: string
+  email: string
+  password: string
+  perfil_logo: string | null
 }
