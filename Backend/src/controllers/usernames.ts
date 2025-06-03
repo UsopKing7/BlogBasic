d894f66119dbe74f9fff71fb39fc16a6aa37d3d4
UsopKing7 <programadorusop@gmail.com>
2025-06-02 22:38:45 -0400

    enpoind para actualizar usuario

--- Backend/src/controllers/usernames.ts
+++ Backend/src/controllers/usernames.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { pool } from '../models/db'
-import { UsuarioConsulta } from '../config'
+import { UsuarioConsulta, UpdateUsuario } from '../config'
+import { validacionUsuarioUpdate } from '../routers/validaciones'
 
 export const routerUsername = Router()
 
@@ -18,6 +19,7 @@ routerUsername.get('/usuarios', async (_req, res) => {
     res.status(200).json({
       message: 'Usuarios encontrados',
       data: usuarios.map((user) => ({
+        id: user.id,
         username: user.username,
         email: user.email,
         fechaRegistro: user.registrado_en
@@ -57,6 +59,37 @@ routerUsername.get('/usuario/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: 'Error interno de el servidor',
+      error: error instanceof Error ? error.message : error
+    })
+  }
+})
+
+routerUsername.patch('/usuario/update/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const vUsuarioUpdate: UpdateUsuario = validacionUsuarioUpdate.parse(req.body)
+    const { rows: usuarioExiste } = await pool.query<UsuarioConsulta>(
+      'SELECT * FROM usuarios WHERE id = $1', [id]
+    )
+
+    if (usuarioExiste.length === 0) {
+      res.status(404).json({ meesage: 'El usuario no existe' })
+      return
+    }
+
+    await pool.query(
+      'UPDATE usuarios SET username = $1, email = $2, perfil_logo = $3', [
+        vUsuarioUpdate.username,
+        vUsuarioUpdate.email,
+        vUsuarioUpdate.perfil_logo
+      ]
+    )
+
+    res.status(200).json({ message: 'Usuario actualizado correctamente' })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error interno en el servidor',
       error: error instanceof Error ? error.message : error
     })
   }
