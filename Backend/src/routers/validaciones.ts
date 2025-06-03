722f2c95f8738c3d1ed11382b5ed7cc31943af7a
UsopKing7 <programadorusop@gmail.com>
2025-06-02 22:38:24 -0400

    validacion zod para usuario

--- Backend/src/routers/validaciones.ts
+++ Backend/src/routers/validaciones.ts
@@ -63,3 +63,27 @@ export const validacionRecuperar = z.object({
     .min(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
     .max(36, { message: 'La contraseña no debe superar los 36 caracteres' })
 })
+
+export const validacionUsuarioUpdate = z.object({
+  username: z
+    .string()
+    .min(3, {
+      message: 'El nombre de usuario debe tener al menos 3 caracteres'
+    })
+    .max(20, {
+      message: 'El nombre de usuario no debe superar los 20 caracteres'
+    })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message:
+        'El nombre de usuario solo puede contener letras, números y guiones bajos'
+    }).optional(),
+  email: z
+    .string()
+    .min(5, { message: 'El email debe tener al menos 5 caracteres' })
+    .max(50, { message: 'El email no debe superar los 50 caracteres' })
+    .email({ message: 'El email no es válido' }).optional(),
+  perfil_logo: z
+    .string()
+    .url({ message: 'El logo de perfil debe ser una URL válida' })
+    .optional()
+})
