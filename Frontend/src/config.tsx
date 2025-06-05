d0e7b5b792aca12b982f43d8b324d02e6edc6a00
UsopKing7 <programadorusop@gmail.com>
2025-06-05 18:54:39 -0400

    refactor: agregar hook useAuthStatus para gestionar el estado de autenticación

--- Frontend/src/config.tsx
+++ Frontend/src/config.tsx
@@ -27,3 +27,34 @@ export const useCerrar = () => {
   }
   return { cerraSession }
 }
+
+import { useEffect, useState } from 'react'
+
+export const useAuthStatus = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch('http://localhost:3333/api/check-auth', {
+          credentials: 'include',
+        })
+
+        if (res.ok) {
+          setIsAuthenticated(true)
+        } else {
+          setIsAuthenticated(false)
+        }
+      } catch {
+        setIsAuthenticated(false)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    checkAuth()
+  }, [])
+
+  return { isLoading, isAuthenticated }
+}
