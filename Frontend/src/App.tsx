7f72932b86e060ce3e6c127b40cf848a067c2e8f
UsopKing7 <programadorusop@gmail.com>
2025-06-05 18:54:17 -0400

    refactor: agregar rutas protegidas para perfil y creación de posts

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import { Inicio } from './inicio/inicioHTML'
 import { Register } from './login-register/Register'
 import { Login } from './login-register/Login'
+import ProtectedRoute from './routers/ProtectedRouter'
+import { Perfil } from './perfil/Perfil'
+import { CrearPost } from './posts/PostsNew'
 
 export const App = () => {
   return (
@@ -9,6 +12,22 @@ export const App = () => {
       <Route path="/" element={<Inicio />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route
+        path="/perfil/:id"
+        element={
+          <ProtectedRoute>
+            <Perfil />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/new-post/:id"
+        element={
+          <ProtectedRoute>
+            <CrearPost />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   )
 }
