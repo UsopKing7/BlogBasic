50ad9eea8fd5cd5864df343da0aade4b11abbec9
UsopKing7 <programadorusop@gmail.com>
2025-06-04 21:04:02 -0400

    agregamos en cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -8,7 +8,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({ origin: true }))
+app.use(cors({ 
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 app.use(midelware)
 
