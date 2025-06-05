430b10ae69c210a2f4888867bdfa5a219e228b15
UsopKing7 <programadorusop@gmail.com>
2025-06-04 20:23:56 -0400

    agregar ruta para el inicio de sesión en el componente App

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Inicio } from './inicio/inicioHTML'
 import { Register } from './login-register/Register'
+import { Login } from './login-register/Login'
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
