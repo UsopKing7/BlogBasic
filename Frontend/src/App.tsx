04c98b9999dfa38797103e33a1031c7c4844d2c4
UsopKing7 <programadorusop@gmail.com>
2025-06-04 20:07:00 -0400

    agregar ruta para el registro de usuarios en el componente App

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import { Inicio } from "./inicio/inicioHTML";
+import { Route, Routes } from 'react-router-dom'
+import { Inicio } from './inicio/inicioHTML'
+import { Register } from './login-register/Register'
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
