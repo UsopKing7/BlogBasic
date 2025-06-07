cf406e9d77b28ff4432714020a4a47ebfad9e684
UsopKing7 <programadorusop@gmail.com>
2025-06-07 17:41:49 -0400

    agregamos la ruta para los likes

--- Backend/src/routers/midelware.ts
+++ Backend/src/routers/midelware.ts
@@ -1,10 +1,12 @@
-import { Router } from "express"
+import { Router } from 'express'
 import { routerLoginRegister } from '../controllers/login-register'
 import { routerUsername } from '../controllers/usernames'
 import { routerPosts } from '../controllers/posts'
+import { routerLikes } from '../controllers/likes'
 
 export const midelware = Router()
 
 midelware.use('/api', routerLoginRegister) //login register
 midelware.use('/api', routerUsername)
 midelware.use('/api', routerPosts)
+midelware.use('/api', routerLikes)
