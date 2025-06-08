2b0e8d573d3b8428c3131d4f05318a1d32aa69f0
UsopKing7 <programadorusop@gmail.com>
2025-06-08 16:25:31 -0400

    agregar manejo de likes por post y función para dar like

--- Frontend/src/inicio/inicioFetch.tsx
+++ Frontend/src/inicio/inicioFetch.tsx
@@ -1,13 +1,28 @@
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const usePostsMostrar = () => {
-  const [posts, setPosts] = useState<Array<{ titulo: string, contenido: string, creado_en: string, perfil_logo: string, username: string }>>([])
+  const { id } = useParams<{ id: string }>()
+  const [likesPorPost, setLikesPorPost] = useState<{
+    [postId: number]: number
+  }>({})
 
-  useEffect(() => { 
+  const [posts, setPosts] = useState<
+    Array<{
+      id: number
+      titulo: string
+      contenido: string
+      creado_en: string
+      perfil_logo: string
+      username: string
+    }>
+  >([])
+
+  useEffect(() => {
     const posts = async () => {
       try {
         const res = await fetch('http://localhost:3333/api/posts', {
-          method: 'GET',
+          method: 'GET'
         })
 
         if (res.ok) {
@@ -23,6 +38,53 @@ export const usePostsMostrar = () => {
     posts()
   }, [])
 
+  useEffect(() => {
+    const likes = async () => {
+      const nuevosLikes: { [postId: number]: number } = {}
+      for (const post of posts) {
+        try {
+          const res = await fetch(
+            `http://localhost:3333/api/likes/post/${post.id}`,
+            {
+              method: 'GET',
+              credentials: 'include'
+            }
+          )
 
-  return { posts }
+          if (res.ok) {
+            const data = await res.json()
+            nuevosLikes[post.id] = data.data
+          } else {
+            nuevosLikes[post.id] = 0
+          }
+        } catch {
+          nuevosLikes[post.id] = 0
+        }
+      }
+
+      setLikesPorPost(nuevosLikes)
+    }
+
+    if (posts.length > 0) {
+      likes()
+    }
+  }, [posts])
+
+  const hadnleDarLike = async (postId: number) => {
+    const res = await fetch(
+      `http://localhost:3333/api/usuario/like/${id}/post/${postId}`,
+      {
+        method: 'POST',
+        credentials: 'include'
+      }
+    )
+
+    if (res.ok) {
+      console.log('like puestoy')      
+    } else {
+      alert('ya tiene el like puesto no puedes poner otro')
+    }
+  }
+
+  return { posts, likesPorPost, hadnleDarLike }
 }
